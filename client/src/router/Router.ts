72c09d4c364d3f7d191b5fda4c92dc0097f5ac5e
VuThanhSang <zimb240@gmail.com>
2023-09-08 11:26:34 +0700

    register ui

--- client/src/router/Router.ts
+++ client/src/router/Router.ts
@@ -2,11 +2,12 @@ import BlankLayout from "../layout/BlankLayout/blankLayout";
 import DefaultLayout from "../layout/DefaultLayout/defaultLayout";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
 
 const publicRoutes = [
   { path: "/", component: Home, layout: DefaultLayout },
   { path: "/login", component: Login, layout: BlankLayout },
-  { path: "/register", component: Home, layout: BlankLayout },
+  { path: "/signUp", component: SignUp, layout: BlankLayout },
 ];
 const privateRoutes = [{ path: "/", component: Home }];
 export { publicRoutes, privateRoutes };
