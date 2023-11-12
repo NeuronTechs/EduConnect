0b7cf06244ff819373e9755f93865867b5521403
nguyendinhhieu12345 <dinhhieunguyen07@gmail.com>
2023-11-12 17:21:12 +0700

    api complaint

--- backend/src/config/environment.ts
+++ backend/src/config/environment.ts
@@ -6,5 +6,5 @@ export default {
     HOST : process.env.HOST,
     USER : process.env.USER,
     PASSWORD : process.env.PASSWORD,
-    DATABASE : process.env.DATABASE
+    DATABASE : process.env.DATABASE,
 };
