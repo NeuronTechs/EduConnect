f8a09b7b13d2ce17b5e443af9acc9782188b4875
nguyendinhhieu12345 <dinhhieunguyen07@gmail.com>
2023-12-11 23:19:07 +0700

    fix

--- frontend/src/components/OverviewCourses/Tabs/Reviews.tsx
+++ frontend/src/components/OverviewCourses/Tabs/Reviews.tsx
@@ -126,14 +126,45 @@ const Reviews = () => {
             <p className="font-semibold text-6xl ">
               {statistic?.totalStar?.toFixed(1)}
             </p>
-            <Rating
+            {/* <Rating
               unratedColor="amber"
               ratedColor="amber"
               value={
                 statistic?.totalStar ? Math.round(statistic?.totalStar) : 0
               }
               readonly
-            />
+            /> */}
+            <div className="flex items-center space-x-1 my-3">
+              {Array.from({ length: Math.round(statistic?.totalStar) }).map(
+                (_, i) => (
+                  <svg
+                    key={i}
+                    className="w-8 h-8 text-yellow-300"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                )
+              )}
+
+              {Array.from({ length: 5 - Math.round(statistic?.totalStar) }).map(
+                (_, i) => (
+                  <svg
+                    key={i}
+                    className="w-6 h-6 text-gray-300 dark:text-gray-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                )
+              )}
+            </div>
             <p className="text-sm ">Điểm khóa học</p>
           </div>
           <div className="w-[80%] flex flex-col items-center justify-center">
