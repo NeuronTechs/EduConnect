f0f783e68c290d4b78150378912605676b883763
Nguyen van tu <tu30380@gmail.com>
2023-12-16 00:15:43 +0700

    Fix UI rendering issue and update authentication flow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import DefaultLayout from "./layout/DefaultLayout/defaultLayout";
 import { setupInterceptor } from "./utils/interceptor";
 import { publicRoutes } from "./router/Router";
-import { AppDispatch, store } from "./redux/store";
+import { AppDispatch, RootState, store } from "./redux/store";
 import { useDispatch, useSelector } from "react-redux";
 import { configRouter } from "./configs/router";
 import { Elements } from "@stripe/react-stripe-js";
@@ -12,6 +12,7 @@ import React, { FC } from "react";
 import RequireAuth from "./router/RequireAuth";
 import { AnimatePresence } from "framer-motion";
 import LoadingPage from "./components/LoadingPage/LoadingPage";
+import { User } from "./type";
 interface LayoutProps {
   children?: React.ReactNode;
 }
@@ -21,9 +22,8 @@ function App() {
   // const location = useLocation();
   setupInterceptor(store, dispatch);
   const routerCheck = publicRoutes;
-  const useCurrentUser = useSelector(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (state: any) => state.authSlice.currentUser
+  const useCurrentUser = useSelector<RootState, User>(
+    (state) => state.authSlice.currentUser as User
   );
 
   return (
--- frontend/src/router/RequireAuth.tsx
+++ frontend/src/router/RequireAuth.tsx
@@ -3,67 +3,79 @@ import { User } from "@/type";
 import { Navigate, useLocation } from "react-router-dom";
 interface Props {
   children: JSX.Element;
-  requiredRole?: string | null;
-  user: User | null;
+  requiredRole: string | null;
+  user?: User;
 }
 
 export default function RequireAuth({ children, requiredRole, user }: Props) {
   const location = useLocation();
   // redirects to login if not login
-  if (user === null) {
-    if (requiredRole !== null) {
+  if (location.pathname !== configRouter.registerInformation && user) {
+    if (user?.role === null) {
+      return (
+        <Navigate
+          to={configRouter.registerInformation}
+          state={{ from: location }}
+          replace
+        />
+      );
+    }
+  }
+  if (requiredRole === null) {
+    if (user) {
+      if (location.pathname === configRouter.login)
+        return (
+          <Navigate to={configRouter.home} state={{ from: location }} replace />
+        );
+      if (location.pathname === configRouter.registerInformation)
+        if (user?.role === null) return children;
+        else
+          <Navigate
+            to={configRouter.home}
+            state={{ from: location }}
+            replace
+          />;
+    } else {
+      if (location.pathname === configRouter.registerInformation)
+        return (
+          <Navigate to={configRouter.home} state={{ from: location }} replace />
+        );
+      return children;
+    }
+
+    return children;
+  } else {
+    if (user) {
+      // redirects to home if not teacher
+      if (requiredRole === "1") {
+        if (user?.role === "1") return children;
+        else
+          return (
+            <Navigate
+              to={configRouter.home}
+              state={{ from: location }}
+              replace
+            />
+          );
+      }
+
+      // redirects to home if not admin
+      if (requiredRole === "2") {
+        if (user?.role === "2") return children;
+        else
+          return (
+            <Navigate
+              to={configRouter.home}
+              state={{ from: location }}
+              replace
+            />
+          );
+      }
+      return children;
+    } else {
       return (
         <Navigate to={configRouter.login} state={{ from: location }} replace />
       );
     }
-    return children;
-  } else {
-    // redirects to home if login
-    if (requiredRole === null) {
-      return (
-        <Navigate to={configRouter.home} state={{ from: location }} replace />
-      );
-    }
-    // redirects to register information if not register information
-    if (user.role === null) {
-      if (requiredRole === "null") {
-        return children;
-      } else {
-        return (
-          <Navigate
-            to={configRouter.registerInformation}
-            state={{ from: location }}
-            replace
-          />
-        );
-      }
-    }
-    // redirects to home if register information
-    if (requiredRole === "null") {
-      return (
-        <Navigate to={configRouter.home} state={{ from: location }} replace />
-      );
-    }
-    // redirects to home if not admin
-    if (requiredRole === "2") {
-      if (user.role === "2") {
-        return children;
-      } else {
-        return (
-          <Navigate to={configRouter.home} state={{ from: location }} replace />
-        );
-      }
-    }
-    // redirects to home if not teacher
-    if (requiredRole === "1") {
-      if (user.role === "1") {
-        return children;
-      } else {
-        return (
-          <Navigate to={configRouter.home} state={{ from: location }} replace />
-        );
-      }
-    }
-    return children;
   }
 }
